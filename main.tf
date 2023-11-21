06929f787f5725e5ee4b9579b2ed891ee983bbef
zerunhu <44351924+zerunhu@users.noreply.github.com>
2023-11-21 15:20:59 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,3 @@ resource "tencentcloud_vpc" "vpc" {
   is_multicast = "false"
   name         = "test-vpc-demo"
 }
-
-resource "tencentcloud_vpc" "vpc1" {
-  cidr_block   = "10.116.32.0/20"
-  is_multicast = "false"
-  name         = "test-vpc-demo1"
-}
